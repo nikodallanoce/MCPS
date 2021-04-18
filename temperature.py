cb2600af2cd0b9d8e161e270b8f1a7bd2cf86fbb
nikodallanoce <dllnik97@gmail.com>
2021-04-18 17:13:15 +0200

    sunday

--- temperature.py
+++ temperature.py
@@ -1,24 +1,29 @@
 import adafruit_dht
+from adafruit_dht import DHT22
 import board
 import time
 
 if __name__ == '__main__':
-    dhtDevice = adafruit_dht.DHT22(board.D4, False)
+    devices = [DHT22(board.D4, False), DHT22(board.D18, False)]
+    temp_hum = []
     read_correctly = False
     for i in range(5):
-        while not read_correctly:
-            try:
-                t, h = dhtDevice.temperature, dhtDevice.humidity
-                read_correctly = True
-                print("Temp={0:0.1f}'C, Humidity={1:0.1f}%".format(t, h))
-                time.sleep(5.0)
+        for i in range(len(devices)):
+            while not read_correctly:
+                try:
+                    t, h = devices[i].temperature, devices[i].humidity
+                    read_correctly = True
+                    #print("Temp={0:0.1f}'C, Humidity={1:0.1f}%".format(t, h))
+                    print("Dev {0} -> temp: {1}'C ___ hum: {2}%".format(i, t, h))
 
-            except RuntimeError as error:
-                # Errors happen fairly often, DHT's are hard to read, just keep going
-                print(error.args[0])
-                time.sleep(2.0)
-            except Exception as error:
-                dhtDevice.exit()
-                raise error
+                except RuntimeError as error:
+                    # Errors happen fairly often, DHT's are hard to read, just keep going
+                    print("Dev {0} -> {1}".format(i, error.args[0]))
+                    time.sleep(2.0)
+                except Exception as error:
+                    devices[i].exit()
+                    raise error
+            read_correctly = False
+        time.sleep(2)
 
-        read_correctly = False
+
